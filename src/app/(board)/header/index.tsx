eddfbda62aa94993f5a279bb31199de16fc3e359
ismaelp96 <ismael6991@live.com>
2026-04-14 19:53:43 -0300

    feat: Client e server components

---
+++ src/app/(board)/header/index.tsx
@@ -0,0 +1,19 @@
+import { SearchInput } from './search-input';
+import { UserButton } from './user-buttont';
+
+export function Header() {
+	return (
+		<div className='w-full max-w-225 mx-auto flex items-center justify-between'>
+			<div className='space-y-1'>
+				<h1 className='font-semibold text-xl'>Product Roadmap</h1>
+				<p className='text-sm text-navy-100'>
+					Follow the development progress of our entire platform
+				</p>
+			</div>
+			<div className='flex items-center gap-4'>
+				<SearchInput />
+				<UserButton />
+			</div>
+		</div>
+	);
+}
---
+++ src/app/(board)/header/search-input.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { ChangeEvent } from 'react';
+import { debounce, parseAsString, useQueryState } from 'nuqs';
+import { SearchIcon } from 'lucide-react';
+
+import { Input } from '@/components/input';
+
+export function SearchInput() {
+	const [search, setSearch] = useQueryState('q', parseAsString.withDefault(''));
+
+	function handleSearchUpdate(e: ChangeEvent<HTMLInputElement>) {
+		setSearch(e.target.value, {
+			limitUrlUpdates: e.target.value !== '' ? debounce(500) : undefined,
+		});
+	}
+	return (
+		<div className='relative'>
+			<SearchIcon className='absolute size-4 text-navy-200 left-2.5 top-1/2 -translate-y-1/2 pointer-events-none' />
+			<Input
+				type='text'
+				placeholder='Search for features...'
+				className='w-67.5 pl-8'
+				value={search}
+				onChange={handleSearchUpdate}
+			/>
+		</div>
+	);
+}
